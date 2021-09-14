af505b42880502bee30450dc5a6fe6e3528dbfe8
kukwa clovis <kukwaclovisngong3@gmail.com>
2021-09-14 23:10:32 +0100

    commiting first exercise

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+let myString = "this is a string";
+// console.log(myString);
+
+// let thisString = myString.split("")
+
+// console.log(thisString)
+// console.log(thisString.length)
+// console.log(myString.length)
+let stringArr = [];
+for(let i = 0; i < myString.length; i++) {
+    stringArr.push(myString[i]);
+}
+
+console.log(stringArr[8]);
+console.log(stringArr.toString())
