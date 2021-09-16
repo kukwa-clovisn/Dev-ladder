12f9845b9bcf7ec1bf6def26d7269f763d984446
kukwa clovis <kukwaclovisngong3@gmail.com>
2021-09-16 21:14:20 +0100

    creating a string and converting it into an array

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,35 @@
 let myString = "this is a string";
 
-let stringArr = [];
+let myStringArr = [];
 for(let i = 0; i < myString.length; i++) {
-    stringArr.push(myString[i]);
+    myStringArr.push(myString[i]);
 }
 
-console.log(stringArr)
+// console.log(myStringArr)
+
+let greetings = "good morning developers"
+
+// creating a function that seperates and and pushes it into an array
+
+function stringArr(greetings) {
+    let Arr = [];
+    Arr[0] = ""
+    Arr[1] = ""
+    Arr[2] = ""
+
+    for(let i = 0; i < greetings.length; i++) {
+        if(i < 4) {
+            Arr[0] += greetings[i]
+        }
+        if(i > 4 && i < 12){
+            Arr[1] += greetings[i]
+        } 
+        if(i > 12) {
+            Arr[2] += greetings[i]
+        }
+    }
+
+    return Arr;
+}
+
+console.log(stringArr(greetings))
